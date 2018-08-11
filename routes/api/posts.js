322d73a947fc29290a35a7c7ac6c472efd635505
Melissa Wolowicz <32224958+m-wolowicz@users.noreply.github.com>
2018-08-11 17:18:39 -0500

    Route Files

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// @route 	GET api/posts/test
+// @desc 	Tests posts route
+// @access 	Public Route
+router.get('/test', (req, res) => res.json({
+	msg: "Posts Works"
+}));
+
+module.exports = router;
