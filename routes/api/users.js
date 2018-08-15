051a4931db497532947e24e54cecb140d3d2e18a
Melissa Wolowicz <32224958+m-wolowicz@users.noreply.github.com>
2018-08-15 18:03:10 -0500

    User login and registration logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 
 //Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User Model
 const User = require('../../models/User');
@@ -72,6 +73,17 @@ router.post('/register', (req, res) => {
 // @route 	GET api/users/login @desc 	Login User / Returning Jason Web Token
 // @access 	Public
 router.post('/login', (req, res) => {
+	const {
+		errors,
+		isValid
+	} = validateLoginInput(req.body);
+
+	//Check Validation
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
+
+
 	const email = req.body.email;
 	const password = req.body.password;
 
@@ -82,9 +94,8 @@ router.post('/login', (req, res) => {
 		.then(user => {
 			//Check for user
 			if (!user) {
-				return res.status(404).json({
-					email: 'User not found'
-				});
+				errors.email = 'User not found';
+				return res.status(404).json(errors);
 			}
 
 			//Check Password
@@ -112,9 +123,8 @@ router.post('/login', (req, res) => {
 								});
 							});
 					} else {
-						return res.status(400).json({
-							password: 'Password incorrect'
-						});
+						errors.password = 'Password incorrect';
+						return res.status(400).json(errors);
 					}
 				})
 		});
--- validation/register.js
+++ validation/register.js
@@ -20,7 +20,32 @@ module.exports = function validateRegisterInput(data) {
 		errors.name = 'Name field is required';
 	}
 
+	if (Validator.isEmpty(data.email)) {
+		errors.email = 'Email field is required';
+	}
 
+	if (!Validator.isEmail(data.email)) {
+		errors.email = 'Email is invalid';
+	}
+
+	if (Validator.isEmpty(data.password)) {
+		errors.password = 'Password field is required';
+	}
+
+	if (!Validator.isLength(data.password, {
+			min: 6,
+			max: 30
+		})) {
+		errors.password = 'Password must be at least 6 characters';
+	}
+
+	if (Validator.isEmpty(data.password2)) {
+		errors.password2 = 'Confirm Password field is required';
+	}
+
+	if (!Validator.equals(data.password, data.password2)) {
+		errors.password2 = 'Passwords must match';
+	}
 
 	return {
 		errors,
