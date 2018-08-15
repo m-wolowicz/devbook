fafc6ec50535f54a75e6f9daf65772969343616b
Melissa Wolowicz <32224958+m-wolowicz@users.noreply.github.com>
2018-08-15 17:46:00 -0500

    Adding Validation

---
+++ validation/is-empty.js
@@ -0,0 +1,17 @@
+// function isEmpty(value) {
+// 	return (
+// 		value === undefined ||
+// 		value === null ||
+// 		(typeof value === 'object' && Object.keys(value).length === 0) ||
+// 		(typeof value === 'string' && value.trim().length === 0)
+// 	);
+// }
+
+//Refactored
+const isEmpty = value =>
+	value === undefined ||
+	value === null ||
+	(typeof value === 'object' && Object.keys(value).length === 0) ||
+	(typeof value === 'string' && value.trim().length === 0);
+
+module.exports = isEmpty;
