05ffdbf38223e46a13467c4ac6556955bffb1b49
Melissa Wolowicz <32224958+m-wolowicz@users.noreply.github.com>
2018-09-02 12:54:40 -0500

    Finished profile.js logic and validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,14 +27,15 @@ router.get('/', passport.authenticate('jwt', {
 	const errors = {};
 
 	Profile.findOne({
-		user: req.user.id
-	}).then(profile => {
-		if (!profile) {
-			errors.noprofile = 'There is no profile for this user';
-			return res.status(404).json(errors);
-		}
-		res.json(profile);
-	}).catch(err => res.status(404).json(err));
+			user: req.user.id
+		}).populate('user', ['name', 'avatar'])
+		.then(profile => {
+			if (!profile) {
+				errors.noprofile = 'There is no profile for this user';
+				return res.status(404).json(errors);
+			}
+			res.json(profile);
+		}).catch(err => res.status(404).json(err));
 });
 
 // @route 	POST api/profile
